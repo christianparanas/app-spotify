286c7fb8aabf2ebc8bcdd80e64dcf6c254561f01
kate <christiannparanas@gmail.com>
2022-01-27 00:01:25 +0800

    implement oauth code flow

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const querystring = require("querystring");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.random() * possible.length);
+  }
 
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get("/callback", (req, res) => {
+  res.send("callback");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
